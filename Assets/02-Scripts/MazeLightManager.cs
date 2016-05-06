fd95f05a7de924e9b561869cf1671b006e76fd4d
AVataRR626 <matt.cabanag@gmail.com>
2016-05-07 00:51:17 +1000

    post force text fix
    resyncing my parts

---
+++ Assets/02-Scripts/MazeLightManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class MazeLightManager : MonoBehaviour
+{
+    public float introTime;
+    public float darknessTime;
+    public float flashTime;
+
+    public int mode;//0 intro time, 1: darkness time, 2: flash time...
+
+    private float clock = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
